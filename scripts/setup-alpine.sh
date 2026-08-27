663e4ca0a41197e2fc5cc1ac2e6b55ced62f3e21
Rohan Bharatia <rohan.bharatia@outlook.com>
2026-08-26 23:46:50 -0400

    Fix install script hostname error

--- scripts/setup-alpine.sh
+++ scripts/setup-alpine.sh
@@ -21,4 +21,4 @@ exit
 # Add basic dependencies
 doas apk add musl bash git vim fastfetch
 
-sh -c 'printf "%s\n" "xset r rate 200 35 &" "xrandr -s 1920x1080" > "$HOME/.xinitrc"'
+sh -c 'printf "%s\n" "xset r rate 200 35 &" "xrandr -s 1920x1080" "rc-update add labd default" "rc-service labd start" > "$HOME/.xinitrc"'
